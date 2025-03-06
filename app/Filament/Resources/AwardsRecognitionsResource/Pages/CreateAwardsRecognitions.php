5420ed83c8f4a2bb01aac0dcfecde81505d7c2f3
Ryan <ryanczaaarq@gmail.com>
2025-03-06 15:26:07 +0800

    awardsrecognition

---
+++ app/Filament/Resources/AwardsRecognitionsResource/Pages/CreateAwardsRecognitions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\AwardsRecognitionsResource\Pages;
+
+use App\Filament\Resources\AwardsRecognitionsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAwardsRecognitions extends CreateRecord
+{
+    protected static string $resource = AwardsRecognitionsResource::class;
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type=image/png href="cpaf-icon.png">
 
         <title>Laravel</title>
+
+        <!-- Add favicon -->
+        <link rel="icon" type="image/png" href="/favicon.ico">
+
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
