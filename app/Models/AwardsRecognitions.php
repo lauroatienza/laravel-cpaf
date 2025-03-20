b99807e73c19c0cbfab2161abb1b4a70b3b4d8a8
Ryan <ryanczaaarq@gmail.com>
2025-03-20 15:35:14 +0800

    okiro

--- app/Models/AwardsRecognitions.php
+++ app/Models/AwardsRecognitions.php
@@ -8,20 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class AwardsRecognitions extends Model
 {
     protected $fillable = [
-<<<<<<< HEAD
-        'award_type',
-        'award_title',
-        'awardee_name',
-        'granting_organization',
-        'date_awarded',
-=======
         "name",
         "award_title",
         "award_desc",
         "award_type",
         "faculty_id",
+        'granting_organization',
         "date_awarded",
->>>>>>> 6e39f4dbe850b99802fb69cd1a3a93d93bbe9157
     ];
 }
 
