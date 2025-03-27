e859dc5c51f7bce2222632619062d6e9a94be829
naki-san <christianilagan2002@gmail.com>
2025-03-27 09:08:32 +0800

    Resolved merge conflicts

--- app/Filament/Resources/ExtensionResource/Pages/CreateExtension.php
+++ app/Filament/Resources/ExtensionResource/Pages/CreateExtension.php
@@ -9,17 +9,14 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateExtension extends CreateRecord
 {
     protected static string $resource = ExtensionResource::class;
-<<<<<<< HEAD
 
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
     }
-=======
     public function getTitle(): string
     {
         return 'Create Extension Involvement'; // Change this to your desired title
     }
     
->>>>>>> e9b222846ef45b24ddea0422ada2ef6c81d6f897
 }
