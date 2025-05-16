64940ca26fc3ca75843881f5ce8e06f813a53b32
CodeHorizon382 <delapazkokoy@gmail.com>
2025-05-16 14:07:04 +0800

    baka may error

--- app/Models/ExtensionPrime.php
+++ app/Models/ExtensionPrime.php
@@ -16,6 +16,29 @@ class ExtensionPrime extends Model
     protected $keyType = 'string'; 
 
     public $timestamps = false;
+
+    public static function booted()
+{
+    static::creating(function ($model) {
+        $model->full_name = self::normalizeName($model->full_name);
+    });
+
+    static::updating(function ($model) {
+        $model->full_name = self::normalizeName($model->full_name);
+    });
+}
+
+protected static function normalizeName($name)
+{
+    $titles = ['Dr.', 'Prof.', 'Engr.', 'Sir', 'Ms.', 'Mr.', 'Mrs.'];
+
+    // Remove titles and normalize spacing
+    $name = str_ireplace($titles, '', $name);
+    $name = preg_replace('/\s+/', ' ', trim($name));
+
+    return $name;
+}
+
     protected $fillable = [
         'name',
         'contributing_unit',
