74535d0e04cec9e09e3f517ce7a41e383914c850
lauroatienza <33475153+lauroatienza@users.noreply.github.com>
2025-06-03 09:49:55 +0800

    Update OrganizedTrainingResource.php

--- app/Filament/Resources/OrganizedTrainingResource.php
+++ app/Filament/Resources/OrganizedTrainingResource.php
@@ -186,7 +186,7 @@ class OrganizedTrainingResource extends Resource
                         TextInput::make('pdf_file_2')->label('PDF File 2')->placeholder('In a single PDF file, maximum of ten (10) survey questionnaires with actual responses or feedback.'),
                         TextInput::make('relevant_documents')->label('Documents Link'),
                         TextInput::make('project_title')
-                            ->label('Project Title')
+                            ->label('Project Title')->placeholder('If under Project, include here the title of the Project.')
 
                     ]),
 
