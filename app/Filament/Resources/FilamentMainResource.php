6a2550ba952bd0c925cab861aa4b4648570c4f9b
lauroatienza <33475153+lauroatienza@users.noreply.github.com>
2025-06-27 16:14:09 +0800

    Update FilamentMainResource.php

--- app/Filament/Resources/FilamentMainResource.php
+++ app/Filament/Resources/FilamentMainResource.php
@@ -154,7 +154,7 @@ class FilamentMainResource extends Resource
 
                     Section::make('Section 2')
                         ->schema([
-                            Textarea::make('research_conference_publisher_details')->label('Research/Conference/Publisher')->placeholder("Do not leave the box blank. Please put 'NA' if you have no answers. Thank you!")->required()->rows(2),
+                            Textarea::make('research_conference_publisher_details')->label('Conference/Publisher')->placeholder("Do not leave the box blank. Please put 'NA' if you have no answers. Thank you!")->required()->rows(2),
                             Textarea::make('study_research_project')->label('Study/Research Project where the publication resulted from')->placeholder('Enter details')->rows(2),
                             Textarea::make('journal_book_conference')->label('Name of Journal/Book/Conference Publication')->placeholder('Enter the name')->rows(2),
                             Textarea::make('publisher_organizer')->label('Name of Organizer')->placeholder('Enter the name')->maxLength(255)->rows(2),
